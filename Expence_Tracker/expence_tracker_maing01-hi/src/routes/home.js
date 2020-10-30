39239181eb9fefa9ef89d44aa39e11fd1e852755
Mick <KondratJs1991@gmail.com>
2020-10-30 16:16:46 +0200

    first change

--- Expence_Tracker/expence_tracker_maing01-hi/src/routes/home.js
+++ Expence_Tracker/expence_tracker_maing01-hi/src/routes/home.js
@@ -98,26 +98,6 @@ export const Home = createVisualComponent({
           </div>
         </GlobalProvider>
 
-        {/*<UU5.Bricks.Row className={CLASS_NAMES.welcomeRow()}>*/}
-        {/*  <UU5.Bricks.Column colWidth="x-12 s-3">*/}
-        {/*    <Plus4U5.Bricks.UserPhoto width="100px" />*/}
-        {/*  </UU5.Bricks.Column>*/}
-        {/*  <UU5.Bricks.Column colWidth="x-12 s-9">*/}
-        {/*    <UU5.Bricks.Header level="2" content={<UU5.Bricks.Lsi lsi={Lsi.auth.welcome} />} />*/}
-        {/*    <UU5.Common.Identity>*/}
-        {/*      {({ identity }) => <UU5.Bricks.Header level="2" content={identity.name} />}*/}
-        {/*    </UU5.Common.Identity>*/}
-        {/*  </UU5.Bricks.Column>*/}
-        {/*</UU5.Bricks.Row>*/}
-        {/*<WelcomeRow textPadding="14px" icon="mdi-human-greeting">*/}
-        {/*  <UU5.Bricks.Lsi lsi={Lsi.auth.intro} />*/}
-        {/*</WelcomeRow>*/}
-        {/*<WelcomeRow textPadding="10px" icon="mdi-monitor">*/}
-        {/*  <UU5.Bricks.Lsi lsi={Lsi.auth.clientSide} />*/}
-        {/*</WelcomeRow>*/}
-        {/*<WelcomeRow textPadding="8px" icon="mdi-server">*/}
-        {/*  <UU5.Bricks.Lsi lsi={Lsi.auth.serverSide} />*/}
-        {/*</WelcomeRow>*/}
       </div>
     );
     //@@viewOff:render
